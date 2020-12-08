187689706eb440305b9b0fbe3d1041d0561136f7
Adigun Mubaraq <adigun.mubaraq06@gmail.com>
2020-12-08 18:03:54 +0100

    Added Filters to find Method

---
+++ mangodb_query.js
@@ -0,0 +1,30 @@
+var MongoClient = require('mongodb');
+const user = "mbq";
+const pass = "mubaraq123";
+const dbname = "animal_farm";
+
+
+var url = `mongodb+srv://${user}:${pass}@cluster0.0mjoi.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var query = { address: "Park Lane 38" }; // filtering the find method 
+  dbo.collection("customers").find(query).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+}); 
+
+// filtering with regular expression 
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    var query = { address: /^S/ }; //outputs results with address that starts with "S"
+    dbo.collection("customers").find(query).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+  }); 
