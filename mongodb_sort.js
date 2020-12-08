86208c76f9d92435edee2ee068587a1e7d8fcacf
Adigun Mubaraq <adigun.mubaraq06@gmail.com>
2020-12-08 18:21:32 +0100

    Added Mongodb Sort Method

---
+++ mongodb_sort.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb');
+const user = "mbq";
+const pass = "mubaraq123";
+const dbname = "animal_farm";
+
+
+var url = `mongodb+srv://${user}:${pass}@cluster0.0mjoi.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var mysort = { name: 1 };
+  dbo.collection("customers").find().sort(mysort).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+}); 
